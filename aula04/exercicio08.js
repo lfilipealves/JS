34789bac1e441f9169ff957e6eca96b608388916
Luis Filipe Alves <lfilipealves20@gmail.com>
2024-12-22 22:20:20 -0300

    Add files via upload

---
+++ aula04/exercicio08.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do meu carro é de ${vel} Km/h`)
+if (vel > 60) { // Condição simples
+    console.log('Você ultrapassou os limites de velocidade, MULTADO!!')
+}
+console.log('Dirija sempre com o cinto de segurança!!')
